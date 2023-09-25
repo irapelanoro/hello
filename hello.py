932938b070030b7a7475fdc4a0c589a1c3c7efc9
irapelanoro <ionenantsoa.rapelanororabenja@epfedu.fr>
2023-09-25 16:25:30 +0200

    Add new version of hello.py

--- hello.py
+++ hello.py
@@ -3,12 +3,12 @@ def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=G-RFNQE146JL"></script>
+src="https://www.googletagmanager.com/gtag/js?id=G-4MN0VH6MBL"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', 'G-RFNQE146JL');
+ gtag('config', 'G-4MN0VH6MBL');
 </script>
  """
  return prefix_google + "Hello World"
