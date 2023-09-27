9f8c546bc996615607b80467dd4009b3006abf99
irapelanoro <ionenantsoa.rapelanororabenja@epfedu.fr>
2023-09-27 21:41:42 +0200

    Add new version of hello.py

--- hello.py
+++ hello.py
@@ -1,28 +1,27 @@
-
-def hello_world():
- prefix_google = """
- <!-- Google tag (gtag.js) -->
-<script async
-src="https://www.googletagmanager.com/gtag/js?id=G-4MN0VH6MBL"></script>
-<script>
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
- gtag('config', 'G-4MN0VH6MBL');
-</script>
- """
- return prefix_google + "Hello World"
-
-
-
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run()
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-4MN0VH6MBL"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-4MN0VH6MBL');
+    </script>
+     """
+    return prefix_google + "Hello World"
+
+
+
+@app.route('/')
+def root():
+    return 'Hello, World!!'
+
+
